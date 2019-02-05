9f6783e725265483c82683cc372012825854089e
wpj980205 <wpj980205@163.com>
2019-02-06 00:10:16 +0800

    167 better

--- 167.两数之和II输入有序数组.cpp
+++ 167.两数之和II输入有序数组.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int idx1, idx2, s=numbers.size();
-        for (idx2=1; idx2<s; idx2++){
-            for (idx1=0; idx1<idx2; idx1++){
-                if (numbers[idx1]+numbers[idx2]==target){
-                    return {idx1+1, idx2+1};
-                }
-            }
+        int idx1=0, idx2=numbers.size()-1 ,sum;
+        while (idx1<idx2){
+            sum=numbers[idx1]+numbers[idx2];
+            if(sum<target)
+                idx1++;
+            else if(sum>target)
+                idx2--;
+            else
+                return {idx1+1, idx2+1};
         }
         return {-1, -1};
     }
