9079100be59c2d9d4a0e8dceaa22ec03f8ae865a
wpj980205 <wpj980205@163.com>
2019-02-05 23:56:02 +0800

    66 better

--- 66.加一.cpp
+++ 66.加一.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        ++digits.back();
-        for(int i=digits.size()-1;i>0;i--){
-            if(digits[i]==10){
-                digits[i-1]++;
-                digits[i]=0;
-            }
+        int idx = digits.size()-1;
+        while (digits[idx]==9){
+            digits[idx]=0;
+            if (idx==0)
+                digits.insert(digits.begin(),0);
+            else
+                --idx;
         }
-        if(digits[0]<10)
-            return digits;
-        else{
-            digits[0]=0;
-            digits.push_back(0);
-            for (int i=digits.size()-1;i>=0;i--)
-                digits[i+1]=digits[i];
-            digits[0]=1;
-            return digits;
-        }
+        digits[idx]++;
+        return digits;
     }
 };
