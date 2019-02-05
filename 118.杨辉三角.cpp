95470631ecd7c40d0b268e510130bfbdf8072148
wpj980205 <wpj980205@163.com>
2019-02-05 23:59:13 +0800

    118

---
+++ 118.杨辉三角.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res(numRows, vector<int>());
+        for (int i=0; i<numRows; i++){
+            res[i].resize(i+1);
+            res[i][0] = 1;
+            res[i][i] = 1;
+        }
+        for (int i=2; i<numRows; i++){
+            for (int j=1; j<res[i].size()-1; j++)
+                res[i][j] = res[i-1][j] + res[i-1][j-1];
+        }
+        return res;
+    }
+};
