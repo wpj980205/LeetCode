50eeba6b1d7c1570680d4d3ebd9f9ca6f83d88a5
wpj980205 <wpj980205@163.com>
2019-02-05 23:54:17 +0800

    max twice

--- 747.至少是其他数字两倍的最大数.cpp
+++ 747.至少是其他数字两倍的最大数.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int dominantIndex(vector<int>& nums) {
-        vector<int>::iterator it = max_element(nums.begin(),nums.end());
-        int max = *it;
-        nums.erase(it);
-        int idx = it - nums.begin();
-        it = max_element(nums.begin(),nums.end());
-        if (max>=2**it)
-            return idx;
+        if(nums.size()==1)
+            return 0;
+        vector<int>::iterator it1 = max_element(nums.begin(),nums.end());
+        int max = *it1;
+        nums.erase(it1);
+        vector<int>::iterator it2 = max_element(nums.begin(),nums.end());
+        if(max>=2**it2)
+            return it1-nums.begin();
         return -1;
     }
 };
