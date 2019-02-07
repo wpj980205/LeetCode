a8ce3c4a45855ec98482d623dc07bdf6dba8d27e
wpj980205 <wpj980205@163.com>
2019-02-07 15:39:44 +0800

    双指针

--- 557. 反转字符串中的单词 III.cpp
+++ 557. 反转字符串中的单词 III.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     string reverseWords(string s) {
         s+=' ';
-        string::iterator a=s.begin(), b=s.begin()+1, e=s.end()-1;
-        while(b<=e){
+        string::iterator a=s.begin(), b=s.begin()+1, e=s.end();
+        while(b<e){
             while(*b!=' ')
                 b++;
             reverse(a, b);
             b++;
             a=b;
         }
-        s.erase(e);
+        s.erase(e-1);
         return s;
     }
 };
