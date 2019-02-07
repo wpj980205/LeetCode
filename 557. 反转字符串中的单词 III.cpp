9095d62ff3802c9af338983981aea6ce4ba042c9
wpj980205 <wpj980205@163.com>
2019-02-07 15:33:39 +0800

    双指针

--- 557. 反转字符串中的单词 III.cpp
+++ 557. 反转字符串中的单词 III.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string::iterator i;
-        vector<string::iterator> vi;
-        s.insert(0, " ");
-        for(i=s.begin(); i!=s.end(); i++){
-            if(*i==' ')
-                vi.push_back(i);
+        s+=' ';
+        string::iterator a=s.begin(), b=s.begin()+1, e=s.end()-1;
+        while(b<=e){
+            while(*b!=' ')
+                b++;
+            reverse(a, b);
+            b++;
+            a=b;
         }
-        vi.push_back(i);
-        int vs = vi.size()-1;
-        for(int i=0; i<vs; i++)
-            reverse(vi[i],vi[i+1]);
-        s.erase(s.end()-1);
-        return s;      
+        s.erase(e);
+        return s;
     }
 };
